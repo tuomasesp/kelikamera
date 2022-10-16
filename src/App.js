c14571e0f8be196331be75f4b34fce825d2f1387
tuomasesp <101732066+tuomasesp@users.noreply.github.com>
2022-10-16 23:34:18 +0300

    api valmis

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
+import axios from 'axios';
 
+const URL = 'https://tie.digitraffic.fi/api/v1/data/camera-data';
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  
+  const [error, setError] = useState(null);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [items, setItems] = useState([])
+  
+  useEffect(() => {
+    
+    const address = URL
+    
+    axios.get(address)
+      .then((response) => { 
+        setError(null);
+        setIsLoaded(true);
+        setItems(response.data.cameraStations[565].cameraPresets)
+      }).catch(error => {
+        console.log(error)
+        alert("Joku vika jossakin.")
+      })
+  }, [])
+  
+  
+  if (error) {
+    return <p>{error.message} </p>
+  }
+  else if (!isLoaded) {
+    return <p>Loading...</p>
+  }
+  else { 
+    return (
+    <div className="content">
+    <h1>Vartiuksen kelikamerat</h1>
+      {items.map(item => ( 
+        <div key={item.id}>
+          <h3>{item.id} {item.presentationName}</h3>
+          <img src={item.imageUrl} alt=''></img>
+        </div>
+      ))}
     </div>
-  );
+    );
+  }
 }
 
 export default App;
